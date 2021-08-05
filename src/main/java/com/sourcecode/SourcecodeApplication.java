06bd11edf497f361319572574d3d12aa83d7c69c
saedjabali <saad.jabali@yahoo.com>
2021-08-05 09:23:32 +0300

    ok

--- src/main/java/com/sourcecode/SourcecodeApplication.java
+++ src/main/java/com/sourcecode/SourcecodeApplication.java
@@ -34,26 +34,26 @@ public class SourcecodeApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        UserAccount userAccount = new UserAccount("Qusay" , "Al-Amarat"  ,
-                "qusay" , passwordEncoder.encode("0000"));
-        UserAccount userAccount1 = new UserAccount("Layana" , "Baba",
-                "layana" , passwordEncoder.encode("0000"));
-
-        UserAccount userAccount2 = new UserAccount("Saad" , "Jabali",
-                "saad" , passwordEncoder.encode("0000"));
-        UserAccount userAccount3 = new UserAccount("Mariam" , "Odat",
-                "mariam" , passwordEncoder.encode("123"));
-        UserAccount userAccount4 = new UserAccount("Mohammad" , "Abu Mattar",
-                "x00125" , passwordEncoder.encode("123"),"My name is Mohammad Abu Mattar, I'm a man_technologist Software Engineer, a graduate in 2018 from \"Isra university\", I wrench build stuff for globe_with_meridians the web.");
-
-
-
-
-        userAccountService.createUserAccount(userAccount);
-        userAccountService.createUserAccount(userAccount1);
-        userAccountService.createUserAccount(userAccount2);
-        userAccountService.createUserAccount(userAccount3);
-        userAccountService.createUserAccount(userAccount4);
+//        UserAccount userAccount = new UserAccount("Qusay" , "Al-Amarat"  ,
+//                "qusay" , passwordEncoder.encode("0000"));
+//        UserAccount userAccount1 = new UserAccount("Layana" , "Baba",
+//                "layana" , passwordEncoder.encode("0000"));
+//
+//        UserAccount userAccount2 = new UserAccount("Saad" , "Jabali",
+//                "saad" , passwordEncoder.encode("0000"));
+//        UserAccount userAccount3 = new UserAccount("Mariam" , "Odat",
+//                "mariam" , passwordEncoder.encode("123"));
+//        UserAccount userAccount4 = new UserAccount("Mohammad" , "Abu Mattar",
+//                "x00125" , passwordEncoder.encode("123"),"My name is Mohammad Abu Mattar, I'm a man_technologist Software Engineer, a graduate in 2018 from \"Isra university\", I wrench build stuff for globe_with_meridians the web.");
+//
+//
+//
+//
+//        userAccountService.createUserAccount(userAccount);
+//        userAccountService.createUserAccount(userAccount1);
+//        userAccountService.createUserAccount(userAccount2);
+//        userAccountService.createUserAccount(userAccount3);
+//        userAccountService.createUserAccount(userAccount4);
 
     }
 
